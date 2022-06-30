c55cf47f30e4076f90b0a01e97a9b2d21d416826
alicendeh <alicendeh16@gmail.com>
2022-06-30 16:03:33 +0100

    Switched to EXPO

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Welcome to Zubhubs Mobile Application</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
